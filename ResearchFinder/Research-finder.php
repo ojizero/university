5265ba056c08fd322ea455b2be5d1613967c39f7
ojizero <z.prince816@yahoo.com>
2016-10-28 12:18:52 +0300

    Added Wordpress plugin project init

---
+++ ResearchFinder/Research-finder.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Research Finder
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A Plugin that takes the users in roles related to research, then starts an external Ruby script to find the researches of those users in Google Scholar, then adds those researches as a custom type (supposed to already exist) named Research.
+Version: 1.0
+Author: oji
+Author URI: https://github.com/ojizero/
+License: GPL2
+*/
+
