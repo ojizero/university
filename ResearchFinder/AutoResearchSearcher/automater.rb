3d56d8279a36c5ec7a71c8dc6f1e63a57753e76c
ojizero <z.acnts.816@protonmail.ch>
2016-11-10 21:39:48 +0200

    Ported input and output handling to standard JSON library. Added indexing logic

--- ResearchFinder/AutoResearchSearcher/automater.rb
+++ ResearchFinder/AutoResearchSearcher/automater.rb
@@ -9,34 +9,53 @@
 ## ############# #LICENSED UNDER THE #MIT LICENSE ############# ##
 ## ############################################################ ##
 
-VERSION_NUMBER = '1.0.1a'
+VERSION_NUMBER = '1.0.1b'
 
+require 'digest/sha2'
 require 'optparse'
 require 'open-uri'
 require 'logger'
-# require 'json'
+require 'json'
+require 'set'
 
+#####################
+## Program globals ##
+#####################
+# Google Scholar link
+# In code adds the query after `q=`
+# add `&num=20` after string to get the max number of results per page Scholar gives.
+BASE_URL                           = 'https://scholar.google.com/scholar?q='
+# default path for input, output, and logging files
+INPUT_FILE                         = './input.users'
+OUTPUT_FILE                        = './output.results'
+LOGGER                             = './logger'
+CROSS_CHECK                        = true
+USE_INDEX                          = false
+INDEX                              = nil
+INDEX_FILE                         = nil
 
 # place website names here, default to parsing the title tag or going to google for other stuff
-$site_names                        = Hash.new nil
-$site_names['ieeexplore.ieee.org'] = 'IEEEXplore'
-$site_names['dl.acm.org']          = 'ACM Digital Library'
-$site_names['www.igi-global.com']  = 'IGI Global'
-$site_names['link.springer.com']   = 'Springer'
-$site_names['springer.com']        = 'Springer'
+$SITE_NAMES                        = Hash.new nil
+$SITE_NAMES['ieeexplore.ieee.org'] = 'IEEEXplore'
+$SITE_NAMES['dl.acm.org']          = 'ACM Digital Library'
+$SITE_NAMES['www.igi-global.com']  = 'IGI Global'
+$SITE_NAMES['link.springer.com']   = 'Springer'
+$SITE_NAMES['springer.com']        = 'Springer'
 
 
 # Main functionality of the program, parses a block of an HTML page producing a Hash map with the results per person
 # @param params Hash contains the parameters used by the function,
-#       :results containing the HTML block with the results,
-#       :check_re (optional) containing the regular expression for validation
-# @return returns an Array containing Hash tables holding the data of each result
+#       :results    containing the HTML block with the results,
+#       :check_re   (optional) containing the regular expression for validation
+# @return returns an Array containing Hash tables holding the data of each result as a first element, and the Sha2 of the titles of the researches if USE_INDEX is enabled (else nil:NilClass)
 def parse_results (params)
 	# Parses results and returns an array for each result as JSON
 	# Function parameters
 	results  = params[:results]
 	check_re = (params[:check_re] or /(.*)/)
-
+	if USE_INDEX
+		newly_added = Set.new
+	end
 	ret = []
 
 	results = results.gsub(/<\/div>|<div id="gs_ccl_results">/, '').strip # remove closing divs
@@ -50,7 +69,9 @@ def parse_results (params)
 		# if result is a user citation
 		# OR
 		# if it's not validated by the string
-		unless result == '' or result =~ /(.*(user profiles for).*|\[citation\])/i or (not result =~ check_re)
+		# OR
+		# if it's already in the index
+		unless result == '' or result =~ /(.*(user profiles for).*|\[citation\])/i or (not result =~ check_re) or (USE_INDEX and INDEX.include? result + "\n")
 			# get the section containing the pdf info and link
 			# remove beginning div tag and trailing div from next
 			pdf_div  = result.match(/<div class="gs_ggs gs_fl">(.*)<div class="gs_ri">/)
@@ -78,37 +99,22 @@ def parse_results (params)
 			# dictionary holding the data
 			res               = {}
 
-			res['__title__']     = publication_title.gsub(/"/, '\"').gsub(/<([^<>]*)>/, '').strip
-			res['__link__']      = publication_link.gsub(/"/, '\"').gsub(/<([^<>]*)>/, '').strip
-			res['__authors__']   = authors_div_arr[0].strip # gsub(/"/, '\"').gsub(/<([^<>]*)>/, '')
-			res['__abstract__']  = abstract_div.gsub(/"/, '\"').gsub(/<([^<>]*)>/, '').strip
-			res['__publisher__'] = ($site_names[authors_div_arr[-1].strip] or authors_div_arr[-1]).strip # .gsub(/"/, '\"').gsub(/<([^<>]*)>/, '')
-			if pdf_link.nil?
-				res['__pdf__'] = nil
-			else
-				res['__pdf__'] = pdf_link.gsub(/"/, '\"').gsub(/<([^<>]*)>/, '').strip
-			end
+			res[:__title__]     = publication_title.gsub(/"/, '\"').gsub(/<([^<>]*)>/, '').strip
+			res[:__hash__]      = Digest::SHA2.hexdigest(result) # Digest::SHA2.hexdigest(res[:__title__])
+			res[:__link__]      = publication_link.gsub(/"/, '\"').gsub(/<([^<>]*)>/, '').strip
+			res[:__authors__]   = authors_div_arr[0].strip # gsub(/"/, '\"').gsub(/<([^<>]*)>/, '')
+			res[:__abstract__]  = abstract_div.gsub(/"/, '\"').gsub(/<([^<>]*)>/, '').strip
+			res[:__publisher__] = ($SITE_NAMES[authors_div_arr[-1].strip] or authors_div_arr[-1]).strip # .gsub(/"/, '\"').gsub(/<([^<>]*)>/, '')
+			pdf_link.nil? ? res[:__pdf__] = nil : res[:__pdf__] = pdf_link.gsub(/"/, '\"').gsub(/<([^<>]*)>/, '').strip
 
 			# append to return variable
 			ret << res
+			newly_added << res[:__hash__] if USE_INDEX
 		end
 	end
 
-	ret
+	return ret, newly_added
 end
-
-#####################
-## Program globals ##
-#####################
-# Google Scholar link
-# In code adds the query after `q=`
-# add `&num=20` after string to get the max number of results per page Scholar gives.
-BASE_URL    = 'https://scholar.google.com/scholar?q='
-# default path for input, output, and logging files
-INPUT_FILE  = './input.users'
-OUTPUT_FILE = './output.results'
-LOGGER      = './logger'
-CROSS_CHECK = true
 
 #####################################
 ## HANDLING OPTIONS OF THE PROGRAM ##
@@ -129,12 +135,20 @@ OptionParser.new do |options|
 	end
 
 	options.on('-v', '--version', 'Print version and exit ..') do
-		p "#{VERSION_NUMBER}"
+		puts VERSION_NUMBER
 		exit 0
 	end
 
 	options.on('-noc' '--NOCHECK-USING-LASTNAME', 'To disable the use of the last name as a RegEx based to validate result or not') do
 		CROSS_CHECK = false
+	end
+
+	options.on('-n' '--USE_INDEX INDEX_FILE', 'Whether or not to use an index file to avoid sending data already existent in the database ... Default no index file') do |index_file|
+		f          = open(index_file, 'r')
+		USE_INDEX  = true
+		INDEX      = Set.new(f.readlines)
+		INDEX_FILE = open(index_file, 'a')
+		f.close
 	end
 end.parse!
 logging = Logger.new open(LOGGER, 'a') # Append to old log file instead of overwriting it
@@ -148,92 +162,69 @@ begin
 	logging.info "#{Time.now} -> Began executing ..."
 
 	# Read users names, if failed exit
-	input  = open(INPUT_FILE, 'r')
+	f      = open(INPUT_FILE, 'r')
+	input  = JSON.parse(f.readlines.join)
 	output = open(OUTPUT_FILE, 'w')
+	f.close
 
 	logging.info 'Opened files ...'
 
 	# Data to be written to output file
 	out = {}
+
 	# Foreach user, search on Google Scholar, parse results of the first page and write them to output file
-	input.each_line do |line|
-		unless line =~ /^\s*[\{\}]?\s*$/
-			info = line.split(':')
-			id   = info[0].strip.gsub(/["']/, ''); user = info[1].strip.gsub(/(^["']|["']\s*(,)?\s*$)/, '').strip
+	input.each do |id, user|
+		user.strip!
 
-			logging.info "Processing user #{user} of id #{id} ..."
+		logging.info "Processing user #{user} of id #{id} ..."
 
-			open(BASE_URL + URI::escape(user)) do |page|
-				# get document by reading the page
-				document = page.readlines.join("\n")
+		open(BASE_URL + URI::escape(user)) do |page|
+			# get document by reading the page
+			document = page.readlines.join("\n")
 
-				if document =~ /(.*)did not match any articles(.*)/i
-					logging.info "Done processing #{user} of id #{id} ... no results found ... skipping"
-					break
-				end
+			if document =~ /(.*)did not match any articles(.*)/i
+				logging.info "Done processing #{user} of id #{id} ... no results found ... skipping"
+				break
+			end
 
-				# from document body, get only the results <div>
-				results = document.match(/<div\s+id="gs_ccl_results">(.*)<div\s+id="gs_ccl_bottom">/)
-				if not results.nil?
-					results = results[0]
-				else
-					logging.info "Done processing #{user} of id #{id} ... results was `nil` ... avoided error by skipping"
-					break
-				end
-				results = results.sub(/<div id="gs_ccl_bottom">/, '').strip
+			# from document body, get only the results <div>
+			results = document.match(/<div\s+id="gs_ccl_results">(.*)<div\s+id="gs_ccl_bottom">/)
+			if not results.nil?
+				results = results[0]
+			else
+				logging.info "Done processing #{user} of id #{id} ... results was `nil` ... avoided error by skipping"
+				break
+			end
+			results = results.sub(/<div id="gs_ccl_bottom">/, '').strip
 
-				# parameters of the parsing function
-				params  = {:results => results}
-				if CROSS_CHECK
-					params[:check_re] = /(.*)#{user.split(' ')[-1]}(.*)/i
-				end
+			# parameters of the parsing function
+			params  = {:results => results}
+			if CROSS_CHECK
+				params[:check_re] = /(.*)#{user.split(' ')[-1]}(.*)/i
+			end
+			# parse the results, for specified user as to avoid any misidentification
+			results = parse_results(params)
 
-				# parse the results, for specified user as to avoid any misidentification
-				results = parse_results(params)
+			# prepare what's to be written to output file
+			out[id] = {
+				:__user__       => user,
+				:__researches__ => results[0]
+			}
 
-				# prepare what's to be written to output file
-				out[id] = {
-					:__user__       => user,
-					:__researches__ => results
-				}
-				# out << "\n\t\"#{id}\": {\n\t\t\"__user__\" : \"#{user}\",\n\t\t\"__researches__\" : #{
-				# if results != [] then
-				# 	'['
-				# else
-				# 	'null'
-				# end}"
-				#
-				# # if results returned aren't an empty set, then do processing
-				# if results != []
-				# 	# remove HTML tags requires extra libraries
-				# 	# for simplicity that is left for the PHP side of the code
-				# 	results.each do |res|
-				# 		# made into one liner to avoid breaking the formatting in file when refactoring the code in IDEs
-				# 		# apologies to anyone trying to decipher this lol
-				# 		out << "\n\t\t\t{\n\t\t\t\t\"__title__\" : \"#{res['__title__']}\",\n\t\t\t\t\"__url__\" : \"#{res['__link__']}\",\n\t\t\t\t\"__authors__\" : \"#{res['__authors__']}\",\n\t\t\t\t\"__abstract__\" : \"#{res['__abstract__']}\",\n\t\t\t\t\"__publisher__\" : \"#{res['__publisher__']}\",\n\t\t\t\t\"__pdf__\" : \"#{res['__pdf__']}\"\n\t\t\t},"
-				# 	end
-				#
-				# 	# remove trailing comma
-				# 	# remove quotes around any defaulted null string
-				# 	out = out[0...-1].gsub(/["]__null__["]/, 'null')
-				# 	out << "\n\t\t]"
-				# end
-				#
-				# out << "\n\t},"
-				logging.info "Done processing #{user} of id #{id} ... no issues"
-			end # END of website processing
-			# write data to output file, ignoring trailing comma
-		end # END of line processing
-	end # END of input file processing
-	# Write to output file, avoid the ignore comma from the last iteration
+			# convert set to array, then to string, then replace ',' with LF and remove brackets
+			INDEX_FILE.write results[1].to_a.to_s.gsub(/, /, "\n").gsub(/[\["']/, '').gsub(/\]/, "\n") if USE_INDEX
+			logging.info "Done processing #{user} of id #{id} ... no issues"
+		end # END of website processing
+	end # END of processing
+	# Write to output file
 	output.write JSON.pretty_generate(out) #.to_json # "{\n#{out[0...-1]}\n}\n"
 rescue Exception => e
-	logging.error "#{e.message}, #{e.backtrace}"
+	logging.error "***ERROR*** => #{e.message}, #{e.backtrace}"
 	exit_status = -1
 ensure
 	# Close files
-	input.close
 	output.close
+	INDEX_FILE.close unless INDEX_FILE.nil?
 
 	logging.info "Exiting program status #{exit_status}"
 
